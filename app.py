adafcc7808bbfb0f703e730c4bd6bfe2e762d01a
ZackOverend <zack.overend@outlook.com>
2025-04-16 16:39:37 -0400

    feat: Ported post endpoint for city name

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
+import requests
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+load_dotenv()
+API_KEY = os.getenv("WEATHER_API_KEY")
 
 @app.route("/")
 def home():
     return "🌦️ NovaWeather API is live! "
+
+# Use post method for security
+@app.route("/weather", methods=["POST"])
+def get_weather():
+    data = request.get_json()
+    city = data.get("city")
+
+    if not city:
+        return jsonify({"error": "Missing 'city' in JSON body"}), 400
+
+    # Call OpenWeatherMap API
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        return jsonify({"error": "City not found or API request failed"}), response.status_code
+
+    return jsonify(response.json())
 
 if __name__ == "__main__":
     app.run(debug=True)
